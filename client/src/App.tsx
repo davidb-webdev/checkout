aca35637d002ff14f54856ccff86ddfab3986029
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-04-05 15:36:43 +0200

    move product testing functions to ProductList component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,15 @@
-import { useState, useReducer } from "react";
-import {
-  createCheckoutSession,
-  getProduct,
-  getProducts
-} from "./services/stripeServices";
-import IProduct from "./models/IProduct";
+import { useReducer } from "react";
 import CartReducer from "./reducers/CartReducer";
 import CartContext from "./contexts/CartContext";
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
 
 const App = () => {
-  const [products, setProducts] = useState<IProduct[]>([]);
   const [cart, dispatch] = useReducer(CartReducer, []);
-
-  const handlePayment = async () => {
-    const data = await createCheckoutSession();
-    console.log(data);
-    window.location = data.url;
-  };
-
-  const getProductsHandler = async () => {
-    const data = await getProducts();
-
-    console.log(data);
-    setProducts(data.data);
-  };
-
-  const getProductHandler = async () => {
-    const data = await getProduct("prod_Pr9a1VJg00Mago");
-
-    console.log(data);
-    setProducts([data]);
-  };
 
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
-      <button onClick={handlePayment}>Pay now</button>
-      <button onClick={getProductsHandler}>Get products</button>
-      <button onClick={getProductHandler}>Get product</button>
-
-      <ProductList products={products} />
+      <ProductList />
       <Cart />
     </CartContext.Provider>
   );
--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -1,16 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import IProduct from "../models/IProduct";
 import { CartActionType } from "../reducers/CartReducer";
 import CartContext from "../contexts/CartContext";
+import {
+  createCheckoutSession,
+  getProduct,
+  getProducts
+} from "../services/stripeServices";
 
-interface IProductListProps {
-  products: IProduct[];
-}
-
-const ProductList = ({ products }: IProductListProps) => {
+const ProductList = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
   const { dispatch } = useContext(CartContext);
+
+  const handlePayment = async () => {
+    const data = await createCheckoutSession();
+    console.log(data);
+    window.location = data.url;
+  };
+
+  const getProductsHandler = async () => {
+    const data = await getProducts();
+
+    console.log(data);
+    setProducts(data.data);
+  };
+
+  const getProductHandler = async () => {
+    const data = await getProduct("prod_Pr9a1VJg00Mago");
+
+    console.log(data);
+    setProducts([data]);
+  };
+
   return (
     <div>
+      <button onClick={handlePayment}>Pay now</button>
+      <button onClick={getProductsHandler}>Get products</button>
+      <button onClick={getProductHandler}>Get product</button>
+
       {products.map((product) => (
         <div key={product.name}>
           <img src={product.images[0]} alt={product.name} />
