96067f8676f6e985abf7bab9a4d912f19d6b5b56
David Boström <144777770+davidb-webdev@users.noreply.github.com>
2024-04-05 20:22:16 +0200

    move Cart to Layout

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -2,6 +2,7 @@ import { useReducer } from "react";
 import { Outlet } from "react-router-dom";
 import CartReducer from "../reducers/CartReducer";
 import CartContext from "../contexts/CartContext";
+import Cart from "../components/Cart";
 
 const Layout = () => {
   const [cart, dispatch] = useReducer(CartReducer, []);
@@ -9,6 +10,7 @@ const Layout = () => {
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
       <Outlet />
+      <Cart />
     </CartContext.Provider>
   );
 };
